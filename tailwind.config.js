2912b9840e318e6f9c63cb48c0751a217cb53622
ForestLee0513 <woolimlee.dev@gmail.com>
2023-02-01 18:58:38 +0900

    chore: Define parimary, gray color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#5531e4',
+          200: '#4b34aa',
+          300: '#372580',
+        },
+        gray: {
+          100: '#eeeeee',
+          200: '#e2e2e2',
+          300: '#cccccc',
+          400: '#999999',
+          500: '#777777',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
