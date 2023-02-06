6c9e28dba414d42eb8d7795388a2d5c1453a7d5b
ForestLee0513 <woolimlee.dev@gmail.com>
2023-02-06 16:03:24 +0900

    [chore] Remove dark mode styles, auto detect

--- src/components/dom/Layout.tsx
+++ src/components/dom/Layout.tsx
@@ -6,10 +6,7 @@ const Layout = forwardRef(({ children, ...props }: HTMLAttributes<HTMLDivElement
   useImperativeHandle(ref, () => localRef.current)
 
   return (
-    <div
-      {...props}
-      ref={localRef}
-      className='absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden dom bg-zinc-900 text-gray-50'>
+    <div {...props} ref={localRef} className='absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden dom'>
       {children}
     </div>
   )
