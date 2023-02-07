140539d407b2b37866155a185fd8755ff8b6b726
ForestLee0513 <woolimlee.dev@gmail.com>
2023-02-07 15:52:05 +0900

    [feat] Add color variables in button

--- src/components/dom/Button.tsx
+++ src/components/dom/Button.tsx
@@ -1,22 +1,44 @@
 import { HTMLAttributes } from 'react'
 
-const Button = ({ children, className, ...props }: HTMLAttributes<HTMLButtonElement>) => {
+const buttonColorVariables = {
+  primary: `
+    bg-primary-200
+    hover:bg-primary-100
+    active:bg-primary-300
+    text-white
+    font-[600]
+    disabled:bg-gray-400
+    disabled:text-gray-900
+  `,
+  secondary: `
+    bg-gray-100
+    font-[400]
+    hover:text-primary-200
+    hover:bg-gray-200
+    active:text-primary-200
+    active:bg-gray-300
+    disabled:text-gray-600
+    disabled:bg-gray-100
+  `,
+} as const
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  color: keyof typeof buttonColorVariables
+  disabled?: boolean
+}
+
+const Button = ({ children, color, className, ...props }: ButtonProps) => {
   // Custom className for override styles or define className for tailwind
   const overridedClassName = className || ''
 
   return (
     <button
       className={`
+        ${buttonColorVariables[color]}
         py-[13px]
         px-[46px]
         rounded-lg
-        font-[600]
-        bg-primary-200
-        hover:bg-primary-100
-        active:bg-primary-300
-        disabled:bg-gray-300
-        disabled:text-gray-700
-        text-white
+
         ${overridedClassName}
       `}
       {...props}>
