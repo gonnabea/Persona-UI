5dd3fd30c5fddc19cd6724584a427d63dedaacad
JiwonDev1141 <awesomedev3@rockwonitglobal.com>
2023-03-02 15:28:49 +0900

    [FIX] light 수정

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -2,18 +2,22 @@ import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
 import * as THREE from "three";
 import { Vector3 } from 'three';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Scene({ children, ...props }) {
 
   const targetObject = useRef();
 
+  useEffect(() => {
+
+  })
+
   // Everything defined in here will persist between route changes, only children are swapped
   return (
     <Canvas {...props}>
-      <directionalLight position={[0, 0, 0]} intensity={4} target={targetObject.current} />
+      <directionalLight position={[0, 0, 0]} intensity={15} target={targetObject.current} />
       <object3D ref={targetObject} position={[-4, 0, 0]} />
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.6} />
       {children}
       <Preload all />
       {/* <OrbitControls /> */}
