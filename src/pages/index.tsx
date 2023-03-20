7a86ec8307e39cf12adda691ad9c6347397d544c
ForestLee0513 <woolimlee.dev@gmail.com>
2023-03-21 00:37:51 +0900

    [feat] 토큰 확인 후 존재하면 캐릭터 창으로 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react'
 import Router from 'next/router'
+import { useRecoilState } from 'recoil'
+import auth from '@/recoil/auth/atom'
 
 export default function Page() {
+  const [authState] = useRecoilState(auth)
   useEffect(() => {
     // Recoil 적용하면 로그인 상태 확인 후 캐릭터 선택으로 이동하는 로직 구현 예정
-    Router.push('/signin')
-  }, [])
+    const { accessToken } = authState
+    if (accessToken) {
+      Router.push('/characters')
+    } else {
+      Router.push('/signin')
+    }
+  }, [authState])
 
   return null
 }
