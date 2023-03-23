ebffea7c300b8eaf803a36245bbc3eff3a159b97
ForestLee0513 <woolimlee.dev@gmail.com>
2023-03-24 00:33:47 +0900

    [fix] 캐릭터 선택 내 헤더 배경색 투명 처리

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -14,7 +14,7 @@ const Characters = () => {
 
   return (
     <div className='flex flex-col h-auto min-h-full bg-primary-400'>
-      <Header className='border-none py-[40px]' containerClassName='max-w-full px-[40px]'>
+      <Header className='bg-transparent border-none py-[40px]' containerClassName='max-w-full px-[40px]'>
         <PersonaBI className='fill-white w-[145.71px]' />
       </Header>
       <Container className='flex flex-col items-center justify-center flex-1 [&>*]:text-white'>
