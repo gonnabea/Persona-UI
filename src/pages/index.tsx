7620838b910ddb08bdb8b69a6c5e71049327e44d
ForestLee0513 <woolimlee.dev@gmail.com>
2023-03-30 01:28:03 +0900

    [fix] index 페이지에 접근 시 토큰 유무에 따라 페이지 이동 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
+import { useRecoilState } from 'recoil'
+import authState from '@/recoil/auth/atom'
+
 export default function Page() {
+  const [auth] = useRecoilState(authState)
+
+  useEffect(() => {
+    const { accessToken } = auth
+
+    if (!accessToken) {
+      Router.push('/signin')
+    } else {
+      Router.push('/characters')
+    }
+  }, [auth])
+
   return null
 }
