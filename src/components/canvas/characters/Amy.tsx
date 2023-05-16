8f893750f3078d269ecbac340d9e200b78b68b13
Jiwon <winning1993@naver.com>
2023-05-16 19:59:16 +0900

    [fix] amy 캐릭터 애니메이션 개선

--- src/components/canvas/characters/Amy.tsx
+++ src/components/canvas/characters/Amy.tsx
@@ -11,13 +11,11 @@ import { colyseusRoomState } from '@/recoil/colyseusRoom/atom'
 import { useRecoilState } from 'recoil'
 import { colyseusPlayersState } from '@/recoil/colyseusPlayers/atom'
 import { clone as SkeletonUtilsClone } from "../../../utils/SkeletonUtils";
-
 // GLTF Actions Type
 type ActionName = 'run'
 interface GLTFActions extends THREE.AnimationClip {
   name: ActionName
 }
-
 // GLTF Result Type
 type GLTFResult = GLTF & {
   nodes: {
@@ -29,7 +27,6 @@ type GLTFResult = GLTF & {
   }
   animations: GLTFActions[]
 }
-
 interface propTypes {
   positionX?: number
   positionY?: number
@@ -37,46 +34,31 @@ interface propTypes {
   rotationZ?: number
   isMyCharacter: boolean
 }
-
 function Amy(props: propTypes) {
   const characterRef = useRef<THREE.Group>()
   const groupRef = useRef<THREE.Group>()
   const { nodes, materials, animations, scene: amyScene } = useGLTF('/models/characters/AmyTest.glb') as unknown as GLTFResult
   const { actions } = useAnimations<GLTFActions>(animations, groupRef)
-  const {scene} = useThree()
-  
+  const { scene } = useThree()
   const cloned = useMemo(() => SkeletonUtilsClone(amyScene), [scene])
   const { nodes: clonedNodes } = useGraph(cloned)
-
-
-  const { actions: clonedActions } = useAnimations(animations ,groupRef)
-
+  const { actions: clonedActions } = useAnimations(animations, groupRef)
   // 캐릭터 이동 구현
   const { forward, backward, left, right, jump } = useCharacterControl()
   const [positionX, setPositionX] = useState(-0.3)
   const [positionY, setPositionY] = useState(0.75)
   const [positionZ, setPositionZ] = useState(5)
   const [rotationZ, setRotationZ] = useState(0)
-  
   const [colyseusRoom, setColyseusRoom] = useRecoilState(colyseusRoomState)
   const [colyseusPlayers, setColyseusPlayers] = useRecoilState(colyseusPlayersState);
-  
-  
-  
-
-
-
   const frontVector = new Vector3(0, 0, 0)
   const sideVector = new Vector3(0, 0, 0)
   const direction = new Vector3(0, 0, 0)
-
   let MOVESPEED = 3
-
   const [mesh, api] = useSphere(() => ({
     mass: 1,
     type: 'Dynamic',
     args: [0.2],
-
     onCollideBegin: (e) => {
       if (e.body.name === 'ground1') {
         console.log('바닥과 충돌')
@@ -87,42 +69,32 @@ function Amy(props: propTypes) {
       }
     },
   }))
-
   useEffect(() => {
-
-
   }, [])
-
   const setAnimationStatus = () => {
     if (forward || backward || left || right) {
-      actions['run_in_place']?.play(); actions['standing'].stop();
+      actions['run']?.play(); actions['idle'].stop();
     }
     else {
-      actions['run_in_place']?.stop(); actions['standing'].play(); actions['standing'].time = 15
+      actions['idle'].play();
+      actions['run']?.stop();
     }
   }
-
   useFrame(() => {
     console.log(actions)
     console.log(nodes, materials)
     setAnimationStatus()
-
-    if(props.isMyCharacter === true) {
+    if (props.isMyCharacter === true) {
       console.log(forward, backward, left, right)
-      
       frontVector.set(0, 0, Number(forward) - Number(backward))
       sideVector.set(Number(right) - Number(left), 0, 0)
-  
       direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVESPEED)
       characterRef.current.rotation.z < 1.7 ? (characterRef.current.rotation.z += Number(right) / 5) : null
       characterRef.current.rotation.z > -1.7 ? (characterRef.current.rotation.z -= Number(left) / 5) : null
       characterRef.current.rotation.z > -3.4 ? (characterRef.current.rotation.z -= Number(backward) / 5) : null
       characterRef.current.rotation.z < 0 ? (characterRef.current.rotation.z += Number(forward) / 5) : null
-  
       api.velocity.set(direction.x, 0, direction.z)
-  
       mesh.current.getWorldPosition(characterRef.current.position)
-  
       setPositionX(characterRef.current.position.x)
       setPositionY(characterRef.current.position.y)
       setPositionZ(characterRef.current.position.z)
@@ -137,7 +109,7 @@ function Amy(props: propTypes) {
         positionX,
         positionY,
         positionZ,
-        rotationZ 
+        rotationZ
       })
     }
     else {
@@ -155,20 +127,13 @@ function Amy(props: propTypes) {
       // clonedNodes.Scene.position.z = props.positionZ;
       //             clonedNodes.Scene.rotation.z = props.rotationZ;
     }
-
-    
-    
-    
   })
-
   return props.isMyCharacter ? (
     <>
-      
       {console.log(nodes, materials)}
       <group ref={groupRef} dispose={null}>
         <group
           ref={characterRef}
-       
           scale={[0.01, 0.01, 0.01]} rotation={[Math.PI / 2, 0, 0]} position={[-0.3, 6, 5]}
           onPointerOver={() => {
             document.body.style.cursor = 'pointer'
@@ -178,27 +143,21 @@ function Amy(props: propTypes) {
           }}>
           <ThirdPersonCamera positionX={positionX} positionY={positionY} positionZ={positionZ} rotationZ={rotationZ} />
           <primitive object={nodes.mixamorigHips} />
-          <skinnedMesh geometry={nodes.Mesh022.geometry} material={materials['Ch46_body.022']} skeleton={nodes.Mesh022.skeleton} />
+          <skinnedMesh geometry={nodes.Ch46.geometry} material={materials['Ch46_body.001']} skeleton={nodes.Ch46.skeleton} />
         </group>
       </group>
-        {/* @ts-ignore */}
+      {/* @ts-ignore */}
       <mesh ref={mesh} visible={true}>
         <sphereGeometry args={[0.1]} />
         <meshStandardMaterial color='orange' />
       </mesh>
-      
-      
     </>
-  ) : 
-  <>
-    {console.log(cloned)}
-          
-          <primitive object={cloned} rotation={[Math.PI / 2, 0, rotationZ]} position={[positionX, positionY, positionZ]} scale={0.01}  />
-          {/* <skinnedMesh geometry={clonedNodes.Ch46.geometry} material={materials.Ch46_body} skeleton={clonedNodes.Ch46.skeleton} /> */}
-          {/* <skinnedMesh geometry={clonedNodes.Armature.geometry} material={materials.Armature_body} skeleton={clonedNodes.Armature.skeleton} /> */}
-        
- 
-      </>
+  ) :
+    <>
+      {console.log(cloned)}
+      <primitive object={cloned} rotation={[Math.PI / 2, 0, rotationZ]} position={[positionX, positionY, positionZ]} scale={0.01} />
+      {/* <skinnedMesh geometry={clonedNodes.Ch46.geometry} material={materials.Ch46_body} skeleton={clonedNodes.Ch46.skeleton} /> */}
+      {/* <skinnedMesh geometry={clonedNodes.Armature.geometry} material={materials.Armature_body} skeleton={clonedNodes.Armature.skeleton} /> */}
+    </>
 }
-
 export default Amy
