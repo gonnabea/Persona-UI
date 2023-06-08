53a6e068bfc520776c2ad71feb55f5a8afcf3484
Jiwon <winning1993@naver.com>
2023-06-08 21:53:09 +0900

    [fix] pm2 command not found fixing

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 echo 'deploying dev branch'
 
-pm2 delete persona-ui
+"pm2 delete persona-ui"
 
 git pull origin dev
 
-pm2 start "npm run prod" --name persona-ui
+"pm2 start "npm run prod" --name persona-ui"
