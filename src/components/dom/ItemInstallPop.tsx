2d3bace53de26aa3902a8bcc5590712c5b33f57f
Jiwon <winning1993@naver.com>
2023-06-19 23:44:03 +0900

    [feat] 박스 설치 구현중

--- src/components/dom/ItemInstallPop.tsx
+++ src/components/dom/ItemInstallPop.tsx
@@ -15,13 +15,13 @@ const ItemInstallPop = ({ itemName,  }) => {
 
 
   return (
-    <div className='absolute z-10 p-4 bg-gray-200 right-10 top-24'>
+    <div className='fixed top-0 z-10 p-4 bg-gray-200 right-24'>
         <span className='pb-2 text-lg font-bold '>{itemName}</span>
         <div className="flex flex-col">
             <span>크기 조절</span>
             <div className='flex items-center justify-center'>
                 <span className='pr-2'>{`scaleX: ${selectedItem?.scale.x}`}</span>
-                <input type='range' min={1} max={20} name="scaleX" value={selectedItem ? selectedItem.scale.x : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.scale.x = e.target.value : null} />
+                <input type='range' min={0.1} max={20} name="scaleX" value={selectedItem ? selectedItem.scale.x : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.scale.x = e.target.value : null} />
             </div>
 
             <div className='flex items-center justify-center'>
@@ -31,7 +31,26 @@ const ItemInstallPop = ({ itemName,  }) => {
 
             <div className='flex items-center justify-center'>
                 <span className='pr-2'>{`scaleZ: ${selectedItem?.scale.z}`}</span>
-                <input type='range' min={1} max={20} name="scaleZ" value={selectedItem ? selectedItem.scale.z : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.scale.z = e.target.value : null} />
+                <input type='range' min={0.1} max={20} name="scaleZ" value={selectedItem ? selectedItem.scale.z : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.scale.z = e.target.value : null} />
+            </div>
+
+        </div>
+
+        <div className="flex flex-col">
+            <span>회전 조절</span>
+            <div className='flex items-center justify-center'>
+                <span className='pr-2'>{`rotationX: ${selectedItem?.rotation.x}`}</span>
+                <input step={0.1} type='range' min={0} max={20} name="rotationX" value={selectedItem ? selectedItem.rotation.x : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.rotation.x = e.target.value : null} />
+            </div>
+
+            <div className='flex items-center justify-center'>
+                <span className='pr-2'>{`rotationY: ${selectedItem?.rotation.y}`}</span>
+                <input step={0.1} type='range' min={0} max={10} name="rotationY" value={selectedItem ? selectedItem.rotation.y : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.rotation.y = e.target.value : null} />
+            </div>
+
+            <div className='flex items-center justify-center'>
+                <span className='pr-2'>{`rotationZ: ${selectedItem?.rotation.z}`}</span>
+                <input step={0.1} type='range' min={0} max={20} name="rotationZ" value={selectedItem ? selectedItem.rotation.z : null} onMouseMove={() => forceUpdate(updateIndex + 1)}  onInput={(e) => selectedItem ? selectedItem.rotation.z = e.target.value : null} />
             </div>
 
         </div>
@@ -40,18 +59,24 @@ const ItemInstallPop = ({ itemName,  }) => {
             <span>위치 조정</span>
             <div className='flex items-center justify-center'>
                 <span className='pr-2'>{`positionX: ${selectedItem?.position.x}`}</span>
-                <button onMouseUp={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.x = parseInt(selectedItem.position.x) + 1 : null}>+</button>
-                <button onMouseUp={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.x = parseInt(selectedItem.position.x) - 1 : null}>-</button>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.x = parseFloat(selectedItem.position.x) + 0.1 : null}>+</button>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.x = parseFloat(selectedItem.position.x) - 0.1 : null}>-</button>
 
 
 
             </div>
 
+            <div className='flex items-center justify-center'>
+                <span className='pr-2'>{`positionY: ${selectedItem?.position.y}`}</span>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.y = parseFloat(selectedItem.position.y) + 0.1 : null}>+</button>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.y = parseFloat(selectedItem.position.y) - 0.1 : null}>-</button>
+            </div>
+
     
             <div className='flex items-center justify-center'>
                 <span className='pr-2'>{`positionZ: ${selectedItem?.position.z}`}</span>
-                <button onMouseUp={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.z = parseInt(selectedItem.position.z) + 1 : null}>+</button>
-                <button onMouseUp={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.z = parseInt(selectedItem.position.z) - 1 : null}>-</button>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.z = parseFloat(selectedItem.position.z) + 0.1 : null}>+</button>
+                <button onMouseDown={() => forceUpdate(updateIndex + 1)} onClick={() => selectedItem ? selectedItem.position.z = parseFloat(selectedItem.position.z) - 0.1 : null}>-</button>
             </div>
 
         </div>
