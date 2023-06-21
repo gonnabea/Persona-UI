9ac20b401d368d4d7f58e864e1ea90994b417c59
ForestLee0513 <woolimlee.dev@gmail.com>
2023-06-22 00:11:09 +0900

    [update] 채팅 말풍선 5초 뒤 제거

--- src/components/canvas/ChatBubble.tsx
+++ src/components/canvas/ChatBubble.tsx
@@ -1,7 +1,8 @@
 import { Html } from '@react-three/drei'
+import { useEffect, useState } from 'react'
 
 // 체력바 추가 시 타입추가
-type NamePlateType = {
+type ChatBubbleType = {
   positionX: number
   positionY: number
   positionZ: number
@@ -10,7 +11,22 @@ type NamePlateType = {
 }
 
 // 닉네임 컴포넌트
-const ChatBubble = ({ positionX, positionY, positionZ, text, username }: NamePlateType) => {
+const ChatBubble = ({ positionX, positionY, positionZ, text, username }: ChatBubbleType) => {
+  const [hidden, setHidden] = useState(false)
+
+  useEffect(() => {
+    setHidden(false)
+    const timer = setTimeout(() => {
+      setHidden(true)
+    }, 5000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [text])
+
+  if (hidden) return ''
+
   return (
     <Html position={[positionX, positionY, positionZ]} as='div'>
       <div className='w-auto text-xs text-white border-white rounded-full bg-primary-100 bg-opacity-70 p-[10px] break-keep'>
